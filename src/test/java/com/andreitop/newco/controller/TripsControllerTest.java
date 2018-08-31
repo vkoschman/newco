e2e25a12eca0f6df3dfad1550592b426d7231be9
Andrei Dremov <andrei_dremov@epam.com>
2018-08-31 04:14:51 +0300

    Step 4.1 | Some refactoring

--- src/test/java/com/andreitop/newco/controller/TripsControllerTest.java
+++ src/test/java/com/andreitop/newco/controller/TripsControllerTest.java
@@ -19,7 +19,9 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(TripsController.class)
