bd8f5b013f3dbec543d4069fb3489b49de86f03a
Andrei Dremov <andrei_dremov@epam.com>
2018-09-04 17:03:47 +0300

    Step 7.1 | Some refactoring

--- src/main/java/com/andreitop/newco/App.java
+++ src/main/java/com/andreitop/newco/App.java
@@ -2,10 +2,8 @@ package com.andreitop.newco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
