ef1c0b18404afc6e39fcb56d1c3982228653c679
vkoschman <207955@niuitmo.ru>
2018-09-10 04:15:04 +0400

    pointcuts and advice added

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -1,11 +1,12 @@
 package com.andreitop.newco.aspect;
 
 
+import com.andreitop.newco.dto.TripDto;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -14,7 +15,7 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
-
+    private static long deleteById = 0;
 
     @Before("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
     public void beforeRepoFind(JoinPoint joinPoint) {
@@ -23,4 +24,35 @@ public class LoggingAspect {
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
     }
 
+    @After("com.andreitop.newco.aspect.PointcutContainer.repositoryDelete()")
+    public void afterRepoDelete(JoinPoint joinPoint) {
+        ++deleteById;
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " was called " + deleteById + " times");
+    }
+
+    @Around("com.andreitop.newco.aspect.PointcutContainer.repositoryUpdate()")
+    public Object afterRepoUpdate(ProceedingJoinPoint joinPoint) throws Throwable{
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Intercepting call of: " + className + methodName);
+        long t1 = System.currentTimeMillis();
+        try{
+            Thread.sleep(1000L);
+            return joinPoint.proceed();
+        }finally{
+            long t2=System.currentTimeMillis();
+            logger.info(" ---> Execution of: " + className + methodName + " took: " + (t2-t1)/1000 + " ms.");
+        }
+    }
+
+    @AfterReturning(value="com.andreitop.newco.aspect.PointcutContainer.serviceFindById()", returning = "result")
+    public void afterServiceFindById(JoinPoint joinPoint, TripDto result) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        if (result != null){
+            logger.info(" ---> Method " + className + "." + methodName + " returned a trip by id ");
+        }
+    }
 }
--- src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
+++ src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
@@ -4,7 +4,19 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class PointcutContainer {
 
-    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*())")
+    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*(..))")
     public void repositoryFind() {
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.update(*))")
+    public void repositoryUpdate() {
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.delete(*))")
+    public void repositoryDelete() {
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.service.*Service+.find*(*))")
+    public void serviceFindById() {
     }
 }
